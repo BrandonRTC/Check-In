60d61267f3190d47648e0e5bff23318a85d7aab7
Chas McMahon <cmcmahon2@fordham.edu>
2015-04-22 16:54:54 -0400

    fixes tour tracker query for production

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -4,12 +4,8 @@ class HousesController < ApplicationController
 
 	# for monitoring tours
 	def index
-		# time = Time.now.change({hour: 19})
-		
-
-		# queries for testing
-		# time = Time.new(2015, 2, 20, 12)
-		# @houses = House.includes(:tours).where(tours: {created_at: time..(time + 12.hours)})
+		time = Time.now.change({hour: 23})
+		@houses = House.includes(:tours).where(tours: {created_at: time..(time + 9.hours)})
 	end
 
 end
--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -18,11 +18,4 @@ class CheckIn < ActiveRecord::Base
 	# 		errors.add(:room_id, "Room must be in your building!")
 	# 	end
 	# end
-	
-	# def validate_new_room
-	# 	check = self
-	# 	if self.tour.check_ins.find_index { |el| el.room_id == check.room_id }
-	# 		errors.add(:room_id, "Can't repeat room")
-	# 	end
-	# end
 end
